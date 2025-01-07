3ab6ea90d4aebdf6f02e9707ff20da7d371c9508
Babatunde Olowoyeye <tunex4ind@gmail.com>
2025-01-07 11:48:17 +0100

    Single responsibility principle

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,27 +1,68 @@
-class Product {
-  constructor(
-    public id: string,
-    public price: number,
-    public description: string
-  ) {}
+// Single Responsibility Principle
+// "A class should have only one reason to change." -- Robert C. Martin
 
-  public display(): void {
-    console.log("Display product");
+class BlogPost {
+  title: string;
+  content: string;
+
+  constructor(title: string, content: string) {
+    this.title = title;
+    this.content = content;
+  }
+
+  // Methods related to content management
+  createPost() {
+    // Implementation here
+  }
+
+  updatePost() {
+    // Implementation here
+  }
+
+  deletePost() {
+    // Implementation here
+  }
+
+  // Method related to post display
+  displayHTML() {
+    return `<h1>${this.title}</h1><p>${this.content}</p>`;
   }
 }
-/**Book class extends the Product class */
-class Book extends Product {
-  constructor(
-    public id: string,
-    public price: number,
-    public description: string,
-    public author: string,
-    public title: string
-  ) {
-    super(id, price, description);
+
+// In the above example, displayHTML has nothing to do with the CRUD operations of blog post. SRP suggests that the displayHTML be implemented in a separate class as it handles a different problem.
+
+// Here is a refactored example
+
+class BlogPosts {
+  title: string;
+  content: string;
+
+  constructor(title: string, content: string) {
+    this.title = title;
+    this.content = content;
   }
 
-  public display(): void {
-    console.log("Display product from child class");
+  // Methods related to content management
+  createPost() {
+    // Implementation here
+  }
+
+  updatePost() {
+    // Implementation here
+  }
+
+  deletePost() {
+    // Implementation here
+  }
+}
+
+class BlogPostsDisplay {
+  blogPost: BlogPosts;
+  constructor(blogPost: BlogPosts) {
+    this.blogPost = blogPost;
+  }
+
+  displayHTML() {
+    return `<h1>${this.blogPost.title}</h1><p>${this.blogPost.content}</p>`;
   }
 }
